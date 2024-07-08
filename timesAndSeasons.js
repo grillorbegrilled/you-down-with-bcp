c3a5f199cd6aba6a369b5ba2f9772fd656cca5fb
foutzjesse <foutzjesse@gmail.com>
2024-07-08 07:05:19 -0400

    Update timesAndSeasons.js

--- timesAndSeasons.js
+++ timesAndSeasons.js
@@ -363,35 +363,6 @@ function getLiturgicalDate(now) {
     return "";
 }
 
-function getSeasonalCollect(week) {
-    switch (week) {
-        case "A1":
-        case "A2":
-        case "A3":
-        case "A4":
-            return makeP("ALMIGHTY God, give us grace that we may cast away the works of darkness, and put upon us the armour of light, now in the time of this mortal life in which thy Son Jesus Christ came to visit us in great humility; that in the last day, when he shall come again in his glorious majesty to judge both the quick and the dead, we may rise to the life immortal; through him who liveth and reigneth with thee and the Holy Ghost, one God, now and for ever. Amen.");
-            break;
-        case "Xmas":
-        case "X1":
-            return makeP("ALMIGHTY God, who hast given us thy only-begotten Son to take our nature upon him, and as at this time to be born of a pure Virgin; Grant that we being regenerate, and made thy children by adoption and grace, may daily be renewed by thy Holy Spirit; through the same our Lord Jesus Christ, who liveth and reigneth with thee and the same Spirit, ever one God, world without end. Amen.");
-            break;
-        case "AW":
-        case "L1":
-        case "L2":
-        case "L3":
-        case "L4":
-        case "L5":
-        case "L6":
-            return makeP("ALMIGHTY and everlasting God, who hatest nothing that thou hast made and dost forgive the sins of all them that are penitent; Create and make in us new and contrite hearts, that we, worthily lamenting our sins, and acknowledging our wretchedness, may obtain of thee, the God of all mercy, perfect remission and forgiveness; through Jesus Christ our Lord. Amen.");
-            break;
-        default:
-            return "";
-            break;
-    }
-}
-
 function getBlackletterDay() {
     //TODO
 }
-
-//Commemoration collects
