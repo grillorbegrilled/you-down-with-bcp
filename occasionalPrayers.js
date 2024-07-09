b1889738b3776e43cbfd1c06e1f784da3b4204a6
grillorbegrilled <foutzjesse@gmail.com>
2024-07-09 11:02:56 -0400

    Update occasionalPrayers.js

--- occasionalPrayers.js
+++ occasionalPrayers.js
@@ -6,4 +6,32 @@ const generalThanksgiving = `ALMIGHTY God, Father of all mercies, we thine unwor
   and that we shew forth thy praise, not only with our lips, but in our lives; by giving up ourselves to thy service, 
   and by walking before thee in holiness and righteousness all our days; 
   through Jesus Christ our Lord, to whom with thee and the Holy Ghost be all honour and glory, world without end. Amen.`;
-const summary = "THOU shalt love the Lord thy God with all thy heart, and with all thy soul, and with all thy mind. This is the first and great commandment. And the second is like unto it; Thou shalt love thy neighbor as thyself. On these two commandments hang all the Law and the Prophets.";
+
+const humbleAccess = `<p>WE do not presume to come to this thy Table, O merciful Lord, trusting in our own righteousness, but in thy manifold and great mercies.<br>
+  We are not worthy so much as to gather up the crumbs under thy Table.<br>
+  But thou art the same Lord, whose property is always to have mercy:<br>
+  Grant us therefore, gracious Lord, so to eat the flesh of thy dear Son Jesus Christ, and to drink his blood,<br>
+  that our sinful bodies may be made clean by his body, and our souls washed through his most precious blood,<br>
+  and that we may evermore dwell in him, and he in us. Amen.</p>`;
+
+const postcommunions = `<p>O LORD and heavenly Father, we thy humble servants entirely desire thy fatherly goodness mercifully to accept this our sacrifice of praise and thanksgiving;
+  most humbly beseeching thee to grant, that by the merits and death of thy Son Jesus Christ, and through faith in his blood, we and all thy whole Church may obtain remission of our sins, and all other benefits of his passion.<br>
+  
+  And here we offer and present unto thee, O Lord, ourselves, our souls and bodies, to be a reasonable, holy, and lively sacrifice unto thee;
+  humbly beseeching thee, that all we, who are partakers of this holy Communion, may be fulfilled with thy grace and heavenly benediction.<br>
+  
+  And although we be unworthy, through our manifold sins, to offer unto thee any sacrifice, yet we beseech thee to accept this our bounden duty and service;
+  not weighing our merits, but pardoning our offences, through Jesus Christ our Lord;
+  by whom, and with whom, in the unity of the Holy Ghost, all honour and glory be unto thee, O Father Almighty, world without end. Amen.</p>
+
+<p>ALMIGHTY and everliving God, we most heartily thank thee, for that thou dost vouchsafe to feed us, who have duly received these holy mysteries, with the spiritual food of the most precious Body and Blood of thy Son our Saviour Jesus Christ;<br>
+  and dost assure us thereby of thy favour and goodness towards us; and that we are very members incorporate in the mystical body of thy Son, which is the blessed company of all faithful people;<br>
+  and are also heirs through hope of thy everlasting kingdom, by the merits of the most precious death and passion of thy dear Son.<br>
+  And we most humbly beseech thee, O heavenly Father, so to assist us with thy grace, that we may continue in that holy fellowship, and do all such good works as thou hast prepared for us to walk in;<br>
+  through Jesus Christ our Lord, to whom, with thee and the Holy Ghost, be all honour and glory, world without end. Amen.</p>`;
+
+const gloriaInExcelsis = `<p>GLORY be to God on high, and in earth peace, good will towards men.<br>
+  We praise thee, we bless thee, we worship thee, we glorify thee, we give thanks to thee for thy great glory, O Lord God, heavenly King, God the Father Almighty.<br>
+  O Lord, the only begotten Son Jesu Christ; O Lord God, Lamb of God, Son of the Father, that takest away the sins of the world, have mercy upon us.<br>
+  Thou that takest away the sins of the world, have mercy upon us. Thou that takest away the sins of the world, receive our prayer. Thou that sittest at the right hand of God the Father, have mercy upon us.<br>
+  For thou only art holy; thou only art the Lord; thou only, O Christ, with the Holy Ghost, art most high in the glory of God the Father. Amen.</p>`;
