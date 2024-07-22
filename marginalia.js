c7d15af5a5e59177f2149dc70f8c3f291158dca6
grillorbegrilled <foutzjesse@gmail.com>
2024-07-22 14:18:38 -0400

    Update marginalia.js

--- marginalia.js
+++ marginalia.js
@@ -1,6 +1,9 @@
 function setBorderImage(now) {
   if (now.getMonth() === 7 || now.getMonth() === 6 && now.getDate() > 25 || now.getMonth() === 8 && now.getDate() < 29) { //Serotinal: St. James to Michaelmas
     document.querySelectorAll('.tiled-border').forEach(element => {
+      if (isFast(now))
+        element.style.backgroundImage = 'url("gillsandberries.png")';
+      else
         element.style.backgroundImage = 'url("blackberries.png")';
     });
   }
