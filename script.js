4947e59025771802fd27a8e3abd1e205ea7be8f7
grillorbegrilled <foutzjesse@gmail.com>
2024-09-12 23:00:04 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function showTab(index) {
+    const tabs = document.querySelectorAll('.tab');
+    const contents = document.querySelectorAll('.tab-content');
+
+    tabs.forEach((tab, i) => {
+        if (i === index) {
+            tab.classList.add('active');
+        } else {
+            tab.classList.remove('active');
+        }
+    });
+
+    contents.forEach((content, i) => {
+        if (i === index) {
+            content.classList.add('active');
+        } else {
+            content.classList.remove('active');
+        }
+    });
+}
