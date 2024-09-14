33243b95eb42e73b1b903093344ab459969a4acb
grillorbegrilled <foutzjesse@gmail.com>
2024-09-14 07:59:48 -0400

    Update canticles.js

--- canticles.js
+++ canticles.js
@@ -28,8 +28,7 @@ function getMorn(day) {
         To give knowledge of salvation unto his people : for the remission of their sins,<br>
         Through the tender mercy of our God : whereby the day-spring from on high hath visited us;<br>
         To give light to them that sit in darkness, and in the shadow of death : and to guide our feet into the way of peace.</p>
-        <p>Glory be to the Father, and to the Son : and to the Holy Ghost;<br>
-        As it was in the beginning, is now, and ever shall be : world without end. Amen.`;
+        <p>Glory be.`;
       break;
     case "AW":
     case "L1":
@@ -50,8 +49,7 @@ function getMorn(day) {
         Blessed art thou that beholdest the depths, and dwellest between the Cherubim: * praised and exalted above all for ever.<br>
         Blessed art thou on the glorious throne of thy kingdom: * praised and exalted above all for ever.<br>
         Blessed art thou in the firmament of heaven: * praised and exalted above all for ever.</p>
-        <p>Glory be to the Father, and to the Son, * and to the Holy Ghost;<br>
-        As it was in the beginning, is now, and ever shall be, * world without end. Amen.`;
+        <p>Glory be.`;
       break;
     case "Easter":
     case "EasterMon":
@@ -64,8 +62,7 @@ function getMorn(day) {
         Christ is risen from the dead : and become the first-fruits of them that slept.<br>
         For since by man came death : by man came also the resurrection of the dead.<br>
         For as in Adam all die : even so in Christ shall all be made alive. 1 Cor. xv. 20.</p>
-        <p>Glory be to the Father, and to the Son : and to the Holy Ghost;<br>
-        As it was in the beginning, is now, and ever shall be : world without end. Amen.`;
+        <p>Glory be.`;
       break;
     case "Xmas":
     case "Stephen":
@@ -109,16 +106,14 @@ function getMorn(day) {
         Praise the Lord, O Jerusalem; * praise thy God, O Sion.<br>
         For he hath made fast the bars of thy gates, * and hath blessed thy children within thee.<br>
         He maketh peace in thy borders, * and filleth thee with the flour of wheat.</p>
-        <p>Glory be to the Father, and to the Son, * and to the Holy Ghost;<br>
-        As it was in the beginning, is now, and ever shall be, * world without end. Amen.`;
+        <p>Glory be.`;
       break;
     default:
       return `O BE joyful in the Lord, all ye lands : serve the Lord with gladness, and come before his presence with a song.<br>
         Be ye sure that the Lord he is God; it is he that hath made us, and not we ourselves : we are his people, and the sheep of his pasture.<br>
         O go your way into his gates with thanksgiving, and into his courts with praise : be thankful unto him, and speak good of his Name.<br>
         For the Lord is gracious, his mercy is everlasting : and his truth endureth from generation to generation.</p>
-        <p>Glory be to the Father, and to the Son : and to the Holy Ghost;<br>
-        As it was in the beginning, is now, and ever shall be : world without end. Amen.`;
+        <p>Glory be.`;
   }
 }
 
@@ -132,6 +127,5 @@ function getEve() {
     He hath put down the mighty from their seat : and hath exalted the humble and meek.<br>
     He hath filled the hungry with good things : and the rich he hath sent empty away.<br>
     He remembering his mercy hath holpen his servant Israel : as he promised to our forefathers, Abraham and his seed, for ever.</p>
-    <p>Glory be to the Father, and to the Son : and to the Holy Ghost;<br>
-    As it was in the beginning, is now, and ever shall be : world without end. Amen.`;
+    <p>Glory be.`;
 }
