79719e79f72d8fdc76e48f852559f9a201a6c8e2
grillorbegrilled <foutzjesse@gmail.com>
2024-09-17 10:56:36 -0400

    Update penitentialOffice.js

--- penitentialOffice.js
+++ penitentialOffice.js
@@ -4,9 +4,9 @@ function getPenitentialOffice(lit, date, weekday) {
       date < 8 && weekday === 5) {//first Friday of each month
     return `<H1>A Penitential Office</H1>
     <P>GOD spake these words, and said:<BR>
-      I am the Lord thy God; Thou shalt have none other gods but me.<br>
+      I. &emsp; I am the Lord thy God; Thou shalt have none other gods but me.<br>
         <!--<b>Lord, have mercy upon us, and incline our hearts to keep this law.</b><br>-->
-      Thou shalt not make to thyself any graven image, nor the likeness
+      II. &emsp; Thou shalt not make to thyself any graven image, nor the likeness
       of any thing that is in heaven above, or in the earth beneath,
       or in the water under the earth; thou shalt not bow down to them,
       nor worship them.<br>
@@ -15,11 +15,11 @@ function getPenitentialOffice(lit, date, weekday) {
       of them that hate me; and show mercy unto thousands in them that
       love me and keep my commandments.</H5>-->
         <!--<b>Lord, have mercy upon us, and incline our hearts to keep this law.</b><br>-->
-      Thou shalt not take the Name of the Lord thy God in vain.<br>
+      III. &emsp; Thou shalt not take the Name of the Lord thy God in vain.<br>
       <!--<H5>for the LORD will not hold him guiltless, that taketh his
       Name in vain.</H5>-->
         <!--<b>Lord, have mercy upon us, and incline our hearts to keep this law.</b><br>-->
-      Remember that thou keep holy the Sabbath-day.<br>
+      IV. &emsp; Remember that thou keep holy the Sabbath-day.<br>
       <!--<H5>Six days shalt thou labour, and do all that thou hast to
       do; but the seventh day is the Sabbath of the LORD thy God. In
       it thou shalt do no manner of work; thou, and thy son, and thy
@@ -29,19 +29,19 @@ function getPenitentialOffice(lit, date, weekday) {
       and rested the seventh day: wherefore the LORD blessed the seventh
       day, and hallowed it.</H5>-->
         <b>Lord, have mercy upon us, and incline our hearts to keep this law.</b></p>
-      <p>Honour thy father and thy mother.<br>
+      <p>V. &emsp; Honour thy father and thy mother.<br>
       <!--<H5>that thy days may be long in the land which the LORD thy
       God giveth thee.</H5>-->
         <!--<b>Lord, have mercy upon us, and incline our hearts to keep this law.</b><br>-->
-      Thou shalt do no murder.<br>
+      VI. &emsp; Thou shalt do no murder.<br>
         <!--<b>Lord, have mercy upon us, and incline our hearts to keep this law.</b><br>-->
-      Thou shalt not commit adultery.<br>
+      VII. &emsp; Thou shalt not commit adultery.<br>
         <!--<b>Lord, have mercy upon us, and incline our hearts to keep this law.</b><br>-->
-      Thou shalt not steal.<br>
+      VIII. &emsp; Thou shalt not steal.<br>
         <!--<b>Lord, have mercy upon us, and incline our hearts to keep this law.</b><br>-->
-      Thou shalt not bear false witness against thy neighbour.<br>
+      IX. &emsp; Thou shalt not bear false witness against thy neighbour.<br>
         <!--<b>Lord, have mercy upon us, and incline our hearts to keep this law.</b><br>-->
-      Thou shalt not covet.<br>
+      X. &emsp; Thou shalt not covet.<br>
       <!--<H5>thy neighbour's house, thou shalt not covet thy neighbour's
       wife, nor his servant, nor his maid, nor his ox, nor his ass,
       nor any thing that is his.</H5>-->
@@ -54,31 +54,8 @@ function getPenitentialOffice(lit, date, weekday) {
       commandment. And the second is like unto it; Thou shalt love
       thy neighbour as thyself. On these two commandments hang all the Law and the Prophets.
     </P>
-      
-    <h2>Psalm 51:1-17</h2>
-    <P>HAVE mercy upon me, O God, after thy great goodness : according to the multitude of thy mercies do away mine offences.<BR>
-      Wash me throughly from my wickedness : and cleanse me from my sin.<BR>
-      For I acknowledge my faults : and my sin is ever before me.<BR>
-      Against thee only have I sinned, and done this evil in thy sight : that thou mightest be justified in thy saying, and clear when thou art judged.<BR>
-      Behold, I was shapen in wickedness : and in sin hath my mother conceived me.<BR>
-      But lo, thou requirest truth in the inward parts : and shalt make me to understand wisdom secretly.<BR>
-      Thou shalt purge me with hyssop, and I shall be clean : thou shalt wash me, and I shall be whiter than snow.<BR>
-      Thou shalt make me hear of joy and gladness: that the bones which thou hast broken may rejoice.<BR>
-      Turn thy face away from my sins : and put out all my misdeeds.<BR>
-      Make me a clean heart, O God : and renew a right spirit within me.<BR>
-      Cast me not away from thy presence : and take not thy holy Spirit from me.<BR>
-      O give me the comfort of thy help again : and stablish me with thy free Spirit.<BR>
-      Then shall I teach thy ways unto the wicked : and sinners shall be converted unto thee.<BR>
-      Deliver me from blood-guiltiness, O God, thou that art the God of my health : and my tongue shall sing of thy righteousness.<BR>
-      Thou shalt open my lips, O Lord : and my mouth shall shew thy praise.<BR>
-      For thou desirest no sacrifice, else would I give it thee : but thou delightest not in burnt-offering.<BR>
-      The sacrifice of God is a troubled spirit : a broken and contrite heart, O God, shalt thou not despise.<BR>
-    </P>
-    <p>Glory be to the Father, and to the Son : and to the Holy Ghost;<BR>
-      As it was in the beginning, is now, and ever shall be : world without end. Amen.
-    </P>
 
-    <p><b>ALMIGHTY God, Father of our Lord Jesus Christ,<BR>
+    <p>ALMIGHTY God, Father of our Lord Jesus Christ,<BR>
     Maker of all things, judge of all men;<BR>
     We acknowledge and bewail our manifold sins and wickedness,<BR>
     Which we, from time to time, most grievously have committed,<BR>
@@ -97,48 +74,22 @@ function getPenitentialOffice(lit, date, weekday) {
     Serve and please thee <BR>
     In newness of life, <BR>
     To the honour and glory of thy Name; <BR>
-    Through Jesus Christ our Lord. Amen.</b></p>
+    Through Jesus Christ our Lord. Amen.</p>
     
-    <p>Let us pray.<BR>
-      O LORD, we beseech thee, mercifully hear our prayers, and spare
-      all those who confess their sins unto thee; that they, whose consciences
-      by sin are accused, by thy merciful pardon may be absolved; through Christ our Lord. Amen.
-    </p>
-    
-    <P><b>Turn thou us, O good Lord, and so shall we be turned.<BR>
-      Be favourable, O Lord,<BR>
-      Be favourable to thy people,<BR>
-      Who turn to thee in weeping, fasting, and praying.<BR>
-      For thou art a merciful God,<BR>
-      Full of compassion,<BR>
-      Long-suffering, and of great pity.<BR>
-      Thou sparest when we deserve punishment,<BR>
-      And in thy wrath thinkest upon mercy.<BR>
-      Spare thy people, good Lord, spare them,<BR>
-      And let not thine heritage be brought to confusion.<BR>
-      Hear us, O Lord, for thy mercy is great,<BR>
-      And after the multitude of thy mercies look upon us;<BR>
-      Through the merits and mediation of thy blessed Son,<BR>
-      Jesus Christ our Lord.<BR>
-      Amen.</b>
-    </P>
-
     <p>Hear what comfortable words our Saviour Christ saith unto all that truly turn to him.<br>
-      COME unto me all that travail and are heavy laden, and I will refresh you. St. Matthew 11.28<br>
-      So God loved the world, that he gave his only-begotten Son, to the end that all that believe in him should not perish, but have everlasting life. St. John 3.16</p>
+      COME unto me all that travail and are heavy laden, and I will refresh you. <i>&mdash;St. Matthew 11:28</i><br>
+      So God loved the world, that he gave his only-begotten Son, to the end that all that believe in him should not perish, but have everlasting life. <i>&mdash;St. John 3:16</i></p>
 
     <p>Hear also what Saint Paul saith.<br>
-      This is a true saying, and worthy of all men to be received, that Christ Jesus came into the world to save sinners. 1 St. Timothy 1.15</p>
+      This is a true saying, and worthy of all men to be received, that Christ Jesus came into the world to save sinners. <i>&mdash;1 St. Timothy 1:15</i></p>
 
     <p>Hear also what Saint John saith.<br>
-      If any man sin, we have an Advocate with the Father, Jesus Christ the righteous; and he is the propitiation for our sins. 1 St. John 2.1</p>
+      If any man sin, we have an Advocate with the Father, Jesus Christ the righteous; and he is the propitiation for our sins. <i>&mdash;1 St. John 2:1</i></p>
 
 <p>BLESSED is he whose unrighteousness is forgiven : and whose sin is covered.<BR>
 Blessed is the man unto whom the Lord imputeth no sin : and in whose spirit there is no guile.<BR>
 I said, I will confess my sins unto the Lord : and so thou forgavest the wickedness of my sin.</p>
-    <p>Glory be to the Father, and to the Son : and to the Holy Ghost;<BR>
-      As it was in the beginning, is now, and ever shall be : world without end. Amen.
-    </P>
+    <p>Glory be.</P>
     
     <p>THE Lord bless us, and keep us. The Lord make his face to shine upon us, and be gracious unto us. The Lord lift up his countenance upon us, and give us peace, both now and evermore. Amen.</p>`;
   }
