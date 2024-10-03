57333cc5e6962d86bd8fda85b0519b1eeb7b5a0e
grillorbegrilled <foutzjesse@gmail.com>
2024-10-03 12:24:08 -0400

    Update officeTab.js

--- officeTab.js
+++ officeTab.js
@@ -34,8 +34,7 @@ function getOffice(now, week, feast) {
         //week in Easter EasterMon EasterTue => "easter/ep.html"
         //week in Xmas X1 => "xmas/ep.html"
         //week in Whitsun Whitmon WhitTue || feast !== "" => "ordinary/ep_festal.html"
-    else if (now.getHours() >= 21 || now.getHours() < 5)
-        return "compline.html";
+    else return "compline.html";
     
     /*  
     //All Conditions
@@ -48,6 +47,4 @@ function getOffice(now, week, feast) {
         result += `<h2>A General Thanksgiving</h2>
             <p>ALMIGHTY God, Father of all mercies, we thine unworthy servants do give thee most humble and hearty thanks for all thy goodness and loving-kindness to us, and to all men; <i>[particularly ________.]</i> We bless thee for our creation, preservation, and all the blessings of this life; but above all, for thine inestimable love in the redemption of the world by our Lord Jesus Christ; for the means of grace, and for the hope of glory. And, we beseech thee, give us that due sense of all thy mercies, that our hearts may be unfeignedly thankful, and that we shew forth thy praise, not only with our lips, but in our lives; by giving up ourselves to thy service, and by walking before thee in holiness and righteousness all our days; through Jesus Christ our Lord, to whom with thee and the Holy Ghost be all honour and glory, world without end. Amen.</p>`;
     */
-    }
-    return result;
 }
