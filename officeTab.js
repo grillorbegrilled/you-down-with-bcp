46dbb147ab593ff377dc40c0ef732fe943899b9e
grillorbegrilled <foutzjesse@gmail.com>
2024-11-13 14:04:40 -0500

    Update officeTab.js

--- officeTab.js
+++ officeTab.js
@@ -8,7 +8,7 @@ function getOffice(now, week, feast) {
         now.getDate() < 8 && day === 5) //first Friday of the month
         document.getElementById("tab-0").innerHTML = commination;
 
-    else if (isEmberDay(week, now.getMonth(), now.getDate(), day) || //ember days
+    else if (//isEmberDay(week, now.getMonth(), now.getDate(), day) || //ember days
         isRogationDay(week, day) || //rogation days
         week === "GF" || //good friday
         week === "AW" && (day === 3 || day === 5) || //ash wednesday, friday thereafter
@@ -84,6 +84,9 @@ function getOffice(now, week, feast) {
             additionalPrayers += `<h2>A General Thanksgiving</h2>
                 <p>ALMIGHTY God, Father of all mercies, we thine unworthy servants do give thee most humble and hearty thanks for all thy goodness and loving-kindness to us, and to all men; <i>[particularly ________.]</i> We bless thee for our creation, preservation, and all the blessings of this life; but above all, for thine inestimable love in the redemption of the world by our Lord Jesus Christ; for the means of grace, and for the hope of glory. And, we beseech thee, give us that due sense of all thy mercies, that our hearts may be unfeignedly thankful, and that we shew forth thy praise, not only with our lips, but in our lives; by giving up ourselves to thy service, and by walking before thee in holiness and righteousness all our days; through Jesus Christ our Lord, to whom with thee and the Holy Ghost be all honour and glory, world without end. Amen.</p>`;
 
+        //Ember Days
+        additionalPrayers += getEmberDayPrayer(week, now.getMonth(), now.getDate(), day);
+        
         if (additionalPrayers) document.getElementById("additional-prayers").innerHTML = additionalPrayers;
     }
 }
