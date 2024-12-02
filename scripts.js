cf4be1908212a575ba8214386ea2ed86c2154401
grillorbegrilled <foutzjesse@gmail.com>
2024-12-02 06:25:44 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -66,17 +66,15 @@ async function getProperFromFile(liturgicalDay, filePath) {
 }
 
 function synthCollects(cotd, week) {
-    const collectP = makeP(makeDropCap(cotd));
-    
     //TODO commemoration
     //TODO eves/vigils (4:00 PM or later)
     
     const seasonal = getSeasonalCollect(week);
 
-    let result = collectP;
+    let result = makeP(makeDropCap(cotd));
 
-    if (seasonal && collectP !== seasonal)
-        result += seasonal;
+    if (seasonal && cotd !== seasonal)
+        result += makeP(seasonal);
     
     return result;
 }
@@ -87,14 +85,14 @@ function getSeasonalCollect(week) {
         case "A2":
         case "A3":
         case "A4":
-            return makeP("ALMIGHTY God, give us grace that we may cast away the works of darkness, and put upon us the armour of light, now in the time of this mortal life in which thy Son Jesus Christ came to visit us in great humility; that in the last day, when he shall come again in his glorious majesty to judge both the quick and the dead, we may rise to the life immortal; through him who liveth and reigneth with thee and the Holy Ghost, one God, now and for ever. Amen.");
+            return "ALMIGHTY God, give us grace that we may cast away the works of darkness, and put upon us the armour of light, now in the time of this mortal life in which thy Son Jesus Christ came to visit us in great humility; that in the last day, when he shall come again in his glorious majesty to judge both the quick and the dead, we may rise to the life immortal; through him who liveth and reigneth with thee and the Holy Ghost, one God, now and for ever. Amen.";
             break;
         case "Xmas":
         case "Stephen":
         case "JohnEvangelist":
         case "Innocents":
         case "X1":
-            return makeP("ALMIGHTY God, who hast given us thy only-begotten Son to take our nature upon him, and as at this time to be born of a pure Virgin; Grant that we being regenerate, and made thy children by adoption and grace, may daily be renewed by thy Holy Spirit; through the same our Lord Jesus Christ, who liveth and reigneth with thee and the same Spirit, ever one God, world without end. Amen.");
+            return "ALMIGHTY God, who hast given us thy only-begotten Son to take our nature upon him, and as at this time to be born of a pure Virgin; Grant that we being regenerate, and made thy children by adoption and grace, may daily be renewed by thy Holy Spirit; through the same our Lord Jesus Christ, who liveth and reigneth with thee and the same Spirit, ever one God, world without end. Amen.";
             break;
         case "AW":
         case "L1":
@@ -103,7 +101,7 @@ function getSeasonalCollect(week) {
         case "L4":
         case "L5":
         case "L6":
-            return makeP("ALMIGHTY and everlasting God, who hatest nothing that thou hast made and dost forgive the sins of all them that are penitent; Create and make in us new and contrite hearts, that we, worthily lamenting our sins, and acknowledging our wretchedness, may obtain of thee, the God of all mercy, perfect remission and forgiveness; through Jesus Christ our Lord. Amen.");
+            return "ALMIGHTY and everlasting God, who hatest nothing that thou hast made and dost forgive the sins of all them that are penitent; Create and make in us new and contrite hearts, that we, worthily lamenting our sins, and acknowledging our wretchedness, may obtain of thee, the God of all mercy, perfect remission and forgiveness; through Jesus Christ our Lord. Amen.";
             break;
         default:
             return "";
