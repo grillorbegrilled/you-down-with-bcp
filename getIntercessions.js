3e0dc07560ab1d6433192609679e6039a65408b7
grillorbegrilled <foutzjesse@gmail.com>
2024-12-20 09:48:30 -0500

    Update getIntercessions.js

--- getIntercessions.js
+++ getIntercessions.js
@@ -5,7 +5,33 @@ function getIntercessions() {
     <p>ALMIGHTY and everlasting God, from whom cometh every good and perfect gift; Send down upon our Bishops, and other Clergy, especially <span name="primates"></span>, and upon the Congregations committed to their charge, the healthful Spirit of thy grace; and, that they may truly please thee, pour upon them the continual dew of thy blessing. Grant this, O Lord, for the honour of our Advocate and Mediator, Jesus Christ. Amen.</p>
     <p>BLESS thy servant <i>N.</i> with health of body and of spirit. Let the hand of thy blessing be upon her head night and day, and support her in all necessities, strengthen her in all temptations, comfort her in all sorrows, and let her be thy servant in all changes; and make us both to dwell with thee for ever in thy favour, in the light of thy countenance, and in thy glory. Amen.<i>⸺Jeremy Taylor</i></p>
     <p>ALMIGHTY God, heavenly Father, who hast blessed us with the joy and care of children; Give us light and strength so to train them, that they may love whatsoever things are true and pure and lovely and of good report, following the example of their Saviour Jesus Christ. Amen.</p>
-    <p>GRANT us, O Lord, a vision of this city, fair as it might be in fulfillment of Thy purpose: a city of justice, where none shall prey upon others; a city of plenty, where greed and poverty shall be done away; a city of brotherhood, where success is founded upon service, and honour is given to nobleness alone; a city of peace, where order shall not rest on force, but on love of all for each and of each for all.  Hear Thou, O Lord, our prayer and pledge of love and service; and hasten the day of the eternal city which cometh down from heaven to earth from Thee; through Jesus Christ our Lord. Amen.<i>⸺after Walter Rausenbusch</i></p>
+    <p>GRANT us, O Lord, a vision of this city, fair as it might be in fulfillment of Thy purpose: a city of justice, where none shall prey upon others; a city of plenty, where greed and poverty shall be done away; a city of brotherhood, where success is founded upon service, and honour is given to nobleness alone; a city of peace, where order shall not rest on force, but on love of all for all; through Jesus Christ our Lord. Amen.<i>⸺after Walter Rausenbusch</i></p>
     <p><i>Pray for any special intentions.</i></p>
-    <p>ASSIST us mercifully, O Lord, in these our supplications and prayers, and dispose the way of thy servants towards the attainment of everlasting salvation; that, among all the changes and chances of this mortal life, they may ever be defended by thy most gracious and ready help; through Jesus Christ our Lord. Amen.</p>`;
+    <p>ASSIST us mercifully, O Lord, in these our supplications and prayers, and dispose the way of thy servants towards the attainment of everlasting salvation; that, among all the changes and chances of this mortal life, they may ever be defended by thy most gracious and ready help; through Jesus Christ our Lord. Amen.</p>
+    <h1>A Litany of Humility</h1>
+    <p>Lord Jesus, meek and humble of heart, Hear me.<br>
+    From the desire to be esteemed, Deliver me, Jesus.<br>
+    From the desire to be loved...<br>
+    From the desire to be extolled...<br>
+    From the desire to be honored...<br>
+    From the desire to be praised...<br>
+    From the desire to be preferred to others...<br>
+    From the desire to be consulted...<br>
+    From the desire to be approved...<br>
+    From the fear of being humiliated...<br>
+    From the fear of being despised...<br>
+    From the fear of being rebuked...<br>
+    From the fear of being slandered...<br>
+    From the fear of being forgotten...<br>
+    From the fear of being ridiculed...<br>
+    From the fear of being wronged...<br>
+    From the fear of being suspected...</p>
+    <p>That others may be loved more than I, Jesus, grant me the grace to desire it.<br>
+    That others may be esteemed more than I...<br>
+    That (in the opinion of the world) others may increase and I may decrease...<br>
+    That others may be chosen and I set aside...<br>
+    That others may be praised and I unnoticed...<br>
+    That others may be preferred to me in everything...<br>
+    That others may become holier than I, provided that I may become as holy as I should...<span style="display: block; text-align: right; font-style: italic;">⸺after Card. Merry del Val</span></p>
+    `;
 }
