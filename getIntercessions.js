4815bd78824afe51e54e92c9f933e1c0d50808a9
grillorbegrilled <foutzjesse@gmail.com>
2025-02-27 08:38:34 -0500

    Update getIntercessions.js

--- getIntercessions.js
+++ getIntercessions.js
@@ -1,21 +1,21 @@
 function getIntercessions() {
-    return `<h1>Intercessions</h1>
-    <p>OUR Father.</p>
-    <p>ALMIGHTY God, whose kingdom is everlasting and power infinite; Have mercy upon this whole land; and so rule the hearts of thy servants <span name="prez"></span> our President, the Governor of this State, and all others in authority, that they, knowing whose ministers they are, may above all things seek thy honour and glory; and that we and all the People, duly considering whose authority they bear, may faithfully and obediently honour them, according to thy blessed Word and ordinance; through Jesus Christ our Lord, who with thee and the Holy Ghost liveth and reigneth ever, one God, world without end. Amen.</p>
-    <p>ALMIGHTY and everlasting God, from whom cometh every good and perfect gift; Send down upon our Bishops, and other Clergy, especially <span name="primates"></span>, and upon the Congregations committed to their charge, the healthful Spirit of thy grace; and, that they may truly please thee, pour upon them the continual dew of thy blessing. Grant this, O Lord, for the honour of our Advocate and Mediator, Jesus Christ. Amen.</p>
-    <p>BLESS thy servant <i>N.</i> with health of body and of spirit. Let the hand of thy blessing be upon her head night and day, and support her in all necessities, strengthen her in all temptations, comfort her in all sorrows, and let her be thy servant in all changes; and make us both to dwell with thee for ever in thy favour, in the light of thy countenance, and in thy glory. Amen.<i>⸺Jeremy Taylor</i></p>
+    return `<p>BLESS thy servant <i>N.</i> with health of body and of spirit. Let the hand of thy blessing be upon her head night and day, and support her in all necessities, strengthen her in all temptations, comfort her in all sorrows, and let her be thy servant in all changes; and make us both to dwell with thee for ever in thy favour, in the light of thy countenance, and in thy glory. Amen.<i>⸺Jeremy Taylor</i></p>
     <p>ALMIGHTY God, heavenly Father, who hast blessed us with the joy and care of children; Give us light and strength so to train them, that they may love whatsoever things are true and pure and lovely and of good report, following the example of their Saviour Jesus Christ. Amen.</p>
     <p>O GOD, Giver of life, hear our prayer for <I>N.</i> and for the child she hath conceived, that, by thy grace, they both may come in safety to a timely and healthy birth, and rejoice in thy gracious providence, through Jesus Christ, who liveth and reigneth with thee and the Holy Spirit, one God, world without end. Amen.</p>
     <p>O HEAVENLY Father, who in thy Word hast given us a vision of that holy City to which the nations of the world bring their glory: 
-Behold and visit, we beseech thee, the <I>City</i> of <I>N.</i>. 
-Renew the bonds of charity that uphold our civic life. Send 
-us honest and able leaders. Deliver us from poverty, prejudice, 
-and oppression, that peace may prevail with righteousness, and 
-justice with mercy. And at the last, bring us to thy Holy City, 
-the new Jerusalem, where we shall know perfect unity and peace; 
-through Jesus Christ our Lord. Amen.</p>
-    <p><i>Pray for any special intentions.</i></p>
+        Behold and visit, we beseech thee, the <I>City</i> of <I>N.</i>. 
+        Renew the bonds of charity that uphold our civic life. Send 
+        us honest and able leaders. Deliver us from poverty, prejudice, 
+        and oppression, that peace may prevail with righteousness, and 
+        justice with mercy. And at the last, bring us to thy Holy City, 
+        the new Jerusalem, where we shall know perfect unity and peace; 
+        through Jesus Christ our Lord. Amen.</p>
+
+<p>And now, brethren, summing up all our petitions, and all our thanksgivings, in the words which Christ hath taught us, we make bold to say,</p>
+<p>OUR Father, who art in heaven, Hallowed be thy Name. Thy kingdom come. Thy will be done on earth, As it is in heaven. Give us this day our daily bread. And forgive us our trespasses, As we forgive those who trespass against us. And lead us not into temptation; But deliver us from evil: For thine is the kingdom, and the power, and the glory, for ever and ever. Amen.</p>
+
     <p>ASSIST us mercifully, O Lord, in these our supplications and prayers, and dispose the way of thy servants towards the attainment of everlasting salvation; that, among all the changes and chances of this mortal life, they may ever be defended by thy most gracious and ready help; through Jesus Christ our Lord. Amen.</p>
+
     <h1>A Litany of Humility</h1>
     <p>Lord Jesus, meek and humble of heart,<br>
     <b>Hear me.</b><br>
@@ -45,4 +45,18 @@ through Jesus Christ our Lord. Amen.</p>
     That others may be preferred to me in everything...<br>
     That others may become holier than I, provided that I may become as holy as I should...<span style="display: block; text-align: right; font-style: italic;">⸺after Card. Merry del Val</span></p>
     `;
+}
+
+function getBidding(lit) {
+    if (["A1", "A2", "A3", "A4"].includes(lit))
+        return `Beloved in Christ, in this season of Advent, let it be our care and delight to prepare ourselves to hear again the message of the Angels, and in heart and mind to go even unto Bethlehem, to see the Babe lying in a manger. Let us read and mark in Holy Scripture the tale of the loving purposes of God from the first days of our disobedience unto the glorious Redemption brought us by his holy Child, and let us look forward to the yearly remembrance of his birth with hymns and songs of praise. But first, let us pray for the needs of his whole world; for peace and goodwill over all the earth; for the mission and unity of the Church for which he died, and especially in this country and within this city. And because this of all things would rejoice his heart, let us at this time remember in his name the poor and the helpless; the hungry and the oppressed; the sick and those who mourn; the lonely and the unloved; the aged and the little children; and all those who know not the Lord Jesus, or who love him not, or who by sin have grieved his heart of love. Lastly, let us remember before God his pure and lowly Mother, and all those who rejoice with us, but upon another shore and in a greater light, that multitude which no one can number, whose hope was in the Word made flesh, and with whom, in this Lord Jesus, we for evermore are one.`;
+
+    return `<p>GOOD Christian People, I bid your prayers for Christ’s holy Catholic Church, the blessed company of all faithful people; that it may please God to confirm and strengthen it in purity of faith, in holiness of life, and in perfectness of love, and to restore to it the witness of visible unity; and more especially for that branch of the same planted by God in this land, whereof we are members; that in all things it may work according to God’s will, serve him faithfully, and worship him acceptably.</p>
+<p>Ye shall pray for the President of these United States, and for the Governor of this State, and for all that are in authority; that all, and every one of them, may serve truly in their several callings to the glory of God, and the edifying and well-governing of the people, remembering the account they shall be called upon to give at the last great day.</p>
+<p>Ye shall also pray for the ministers of God’s Holy Word and Sacraments; for Bishops (and herein more especially for the Bishop of this Diocese), that they may minister faithfully and wisely the discipline of Christ; likewise for all Priests and Deacons (and herein more especially for the Clergy here residing), that they may shine as lights in the world, and in all things may adorn the doctrine of God our Saviour.</p>
+<p>And ye shall pray for a due supply of persons fitted to serve God in the Ministry and in the State; and to that end, as well as for the good education of all the youth of this land, ye shall pray for all schools, colleges, and seminaries of sound and godly learning, and for all whose hands are open for their maintenance; that whatsoever tends to the advancement of true religion and useful learning may for ever flourish and abound.</p>
+<p>Ye shall pray for all the people of these United States, that they may live in the true faith and fear of God, and in brotherly charity one towards another.</p>
+<p>Ye shall pray also for all who travel by land or sea; for all prisoners and captives; for all who are in sickness or in sorrow; for all who have fallen into grievous sin; for all who, through temptation, ignorance, helplessness, grief, trouble, dread, or the near approach of death, especially need our prayers.</p>
+<p>Ye shall also praise God for rain and sunshine; for the fruits of the earth; for the products of all honest industry; and for all his good gifts, temporal and spiritual, to us and to all men.</p>
+<p>Finally, ye shall yield unto God most high praise and hearty thanks for the wonderful grace and virtue declared in all his saints, who have been the choice vessels of his grace and the lights of the world in their several generations; and pray unto God, that we may have grace to direct our lives after their good examples; that, this life ended, we may be made partakers with them of the glorious resurrection, and the life everlasting.</p>`;
 }
