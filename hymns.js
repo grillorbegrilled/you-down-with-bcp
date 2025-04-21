8c65413dae5744f3c91661293cc3bbcd6053e236
grillorbegrilled <foutzjesse@gmail.com>
2025-04-21 06:37:55 -0400

    Update hymns.js

--- hymns.js
+++ hymns.js
@@ -5,7 +5,7 @@ function getHymns(liturgicalDay) {
     else if (day === "Stephen" || day === "JohnEvangelist" || day === "Innocents" || day === "X1" || day === "Circ") day = "Xmas";
     else if (day === "E1") day = "Epiphany";
     else if (day === "HW-Tue" || day === "HW-Wed") day = "HW-Mon";
-    else if (day === "Ea-Tue" || day === "Ea-Mon") day = "Easter";
+    else if (day === "EasterMon" || day === "EasterTue") day = "Easter";
     
     const items = hymns[day];
     var result = "";
