589135e0b73e6056168f61243ecd378e986ef1fd
grillorbegrilled <foutzjesse@gmail.com>
2025-05-11 17:33:45 -0400

    Update hymns.js

--- hymns.js
+++ hymns.js
@@ -1292,7 +1292,46 @@ Where thou leadest we would go,<br>
 walking in thy steps below,<br>
 till before our Father's throne<br>
 we shall know as we are known.`],
-    "Ea3": [""],
+    "Ea3": [`A few more years shall roll,<br>
+A few more seasons come,<br>
+And we shall be with those that rest<br>
+Asleep within the tomb;<br>
+Then, O my Lord, prepare<br>
+My soul for that great day.<br>
+O wash me in Thy precious blood,<br>
+And take my sins away.</p><p>
+A few more suns shall set<br>
+O'er these dark hills of time,<br>
+And we shall be where suns are not<br>
+A far serener clime:<br>
+Then, O my Lord, prepare<br>
+My soul for that blest day.<br>
+O wash me in Thy precious blood,<br>
+And take my sins away.</p><p>
+A few more storms shall beat<br>
+On this wild rocky shore,<br>
+And we shall be where tempests cease,<br>
+And surges swell no more;<br>
+Then, O my Lord, prepare<br>
+My soul for that calm day.<br>
+O wash me in Thy precious blood,<br>
+And take my sins away.</p><p>
+A few more struggles here,<br>
+A few more partings o'er,<br>
+A few more toils, a few more tears,<br>
+And we shall weep no more:<br>
+Then, O my Lord, prepare<br>
+My soul for that bright day.<br>
+O wash me in Thy precious blood,<br>
+And take my sins away.</p><p>
+'Tis but a little while,<br>
+And He shall come again<br>
+Who died that we might live, Who lives<br>
+That we with Him may reign;<br>
+Then, O my Lord, prepare<br>
+My soul for that glad day.<br>
+O wash me in Thy precious blood,<br>
+And take my sins away.`],
     "Ea4": [""],
     "Ea5": [""],
     "Ascension": [""],
