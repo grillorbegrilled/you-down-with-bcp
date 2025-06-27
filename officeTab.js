a04c6338e2d7461dd37b7889e5386c4907ebf516
grillorbegrilled <foutzjesse@gmail.com>
2025-06-27 17:01:04 -0400

    Update officeTab.js

--- officeTab.js
+++ officeTab.js
@@ -64,7 +64,9 @@ function getOffice(now, week, feast) {
                 //    document.getElementById('lesson').innerHTML = `<h2>${lessonContent.cit}</h2><p>${makeDropCap(lessonContent.txt)}</p>`;
                 //});
                 document.getElementById('lesson').style.display = "none";
-                
+                const cantMarg = getOfficeMargin2(now, feast || week);
+                if (cantMarg) document.getElementById("cantMarg").src = cantMarg;
+                else document.getElementById("cantMarg").style.display = "none";
                 document.getElementById("office-collect").innerHTML = "<p><span class='drop-cap'>O</span> GOD, from whom all holy desires, all good counsels, and all just works do proceed; Give unto thy servants that peace which the world cannot give; that both our hearts may be set to obey thy commandments, and also that by thee, we, being defended from the fear of our enemies, may pass our time in rest and quietness; through the merits of Jesus Christ our Saviour. Amen.</p>";
                 break;
             case 3:
