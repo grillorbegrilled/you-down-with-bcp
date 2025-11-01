96b324ccc3f40c8ff23ada8dfd00279ff5c0f336
grillorbegrilled <foutzjesse@gmail.com>
2025-10-31 22:53:29 -0400

    Update timesAndSeasons.js

--- timesAndSeasons.js
+++ timesAndSeasons.js
@@ -377,8 +377,7 @@ function getLiturgicalDate(now) {
         if (day === 18) return "Luke";
         if (day === 28) return "SimonAndJude";
     } else if (month === 10) { //nov
-        if (day === 2 && now.getDay() !== 0 || day === 3 && now.getDay() === 1) return "AllSouls";
-        if (day === 1 || day > 1 && day <= 8 && now.getDay() !== 0) return "AllSaints";
+        if (day === 1 || day > 1 && day <= 8) return "AllSaints";
         if (day === 16) return "Hugh";
         if (day >= 22 && day <= 28 && now.getDay() >= 4 || day === 29 && now.getDay() === 5) return "ThanksgivingDay";
         if (day === 30) return "Andrew";
